439b8a361ac4f5a25e067d6d7ad7e5edf6e6f64e
MikeBra <mhbracewell@gmail.com>
2025-02-20 23:01:07 -0600

    Update Google environment params and general code clean up

--- lib/google-oauth-cdk-stack.ts
+++ lib/google-oauth-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib"
 import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import * as nodejs from "aws-cdk-lib/aws-lambda-nodejs"
 import * as path from "path"
 
 export class GoogleOAuthCdkStack extends cdk.Stack {
@@ -27,17 +28,23 @@ export class GoogleOAuthCdkStack extends cdk.Stack {
 		)
 
 		// Get User Info Lambda
-		const getUserInfoLambda = new lambda.Function(this, "GetUserInfoHandler", {
-			runtime: lambda.Runtime.NODEJS_18_X,
-			handler: "get-user-info.handler",
-			code: lambda.Code.fromAsset(path.join(__dirname, "../src/lambda")),
-			environment,
-		})
+		const getUserInfoFunction = new nodejs.NodejsFunction(
+			this,
+			"GetUserInfoFunction",
+			{
+				runtime: lambda.Runtime.NODEJS_18_X,
+				entry: path.join(__dirname, "../src/lambda/google-get-user-info.ts"),
+				handler: "handler",
+				environment: {
+					GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
+					GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
+				},
+			}
+		)
 
 		// API Gateway
 		const api = new apigateway.RestApi(this, "GoogleOAuthApi", {
-			restApiName: "Google OAuth Service",
-			description: "API Gateway for Google OAuth integration",
+			restApiName: "Google OAuth API",
 		})
 
 		// OAuth callback endpoint
@@ -49,10 +56,19 @@ export class GoogleOAuthCdkStack extends cdk.Stack {
 		)
 
 		// User info endpoint
-		const userInfo = api.root.addResource("userinfo")
+		const userInfo = api.root.addResource("user-info")
 		userInfo.addMethod(
 			"GET",
-			new apigateway.LambdaIntegration(getUserInfoLambda)
+			new apigateway.LambdaIntegration(getUserInfoFunction),
+			{
+				authorizationType: apigateway.AuthorizationType.NONE, // Since we're using Google's tokens
+			}
 		)
+
+		// Output the API URL
+		new cdk.CfnOutput(this, "ApiUrl", {
+			value: api.url,
+			description: "API Gateway URL",
+		})
 	}
 }
